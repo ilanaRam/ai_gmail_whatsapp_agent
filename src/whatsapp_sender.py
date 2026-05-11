bb91325d41d3d3ffb2a0a99c230e1e536a97c061
ilana <ilanaaprilloriram@gmail.com>
2026-05-11 23:04:47 +0300

    slight changes to the whatsapp sender

--- src/whatsapp_sender.py
+++ src/whatsapp_sender.py
@@ -3,12 +3,20 @@ from twilio.rest import Client   # to work with Twilio sandbox to send it whatsa
 from dotenv import load_dotenv
 import os
 
+"""
+This is a whatsapp message sender - is knows only about Whatsapp, it knows nothing about the MCP 
+it will work even if we never connect MCP 
+it is called "Separation of Concerns"
+"""
+
+
 load_dotenv()
+
 
 TWILIO_SID = os.getenv('TWILIO_SID')
 TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
-FROM_TWILIO_WHATSAPP_CONTACT_NUMBER = os.getenv('FROM_TWILIO_WHATSAPP_CONTACT_NUMBER')
-TO_MY_WHATSAPP = os.getenv('TO_MY_WHATSAPP')
+TWILIO_WHATSAPP_CONTACT_NUMBER = os.getenv('TWILIO_WHATSAPP_CONTACT_NUMBER')
+MY_WHATSAPP_CONTACT_NUMBER = os.getenv('MY_WHATSAPP_CONTACT_NUMBER')
 
 
 
@@ -27,11 +35,11 @@ def send_whatsapp(subject,
     print(f"Preparing a message to be send to Twilio sandbox that will be sent by him to whatsapp")
 
     print(f"Message From: {sender}, with Subject: {subject}")
-    message_body = f"📬 New Mail!\nFrom: {sender}\nSubject: {subject} "
+    message_body = f"📬 New Mail arrived!\n   From: {sender}\n   Subject: {subject} "
 
     print(f"Message to be send. Message body: {message_body}")
-    #TODO: suround with try and catch !!!!!!!!!!!!!
+    #TODO: surround with try and catch !!!!!!!!!!!!!
     message = client.messages.create(body=message_body,
-                                     from_=FROM_TWILIO_WHATSAPP_CONTACT_NUMBER,
-                                     to=TO_MY_WHATSAPP)
+                                     from_=TWILIO_WHATSAPP_CONTACT_NUMBER,
+                                     to=MY_WHATSAPP_CONTACT_NUMBER)
     print(f"Message sent! Message SID: {message.sid}")
